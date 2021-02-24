6d9a6955a36094c50f31b52cb34e6afc3ef8ce45
Jerry Wang <jerrywangpico@gmail.com>
2021-02-24 16:32:40 -0500

    Change output of dataset
    The output has changed to image and mask instead of list of spectrum. All image will be either cut or padded to the same size

--- src/Dataset/SpecDataset.py
+++ src/Dataset/SpecDataset.py
@@ -6,10 +6,12 @@ import torch.nn.functional as F
 from torch.utils.data import Dataset
 from torchvision import transforms, utils
 import numpy as np
-from .utils.files import Files
-from .utils.HSI_Analysis import caliColor, getMask, getNDVIHeatmap
-import csv
+from utils.files import Files
+from utils.HSI_Analysis import caliColor, getMask, getNDVIHeatmap
+import pandas as pd
 import yaml
+import os
+import matplotlib.pyplot as plt
 
 class SpecDataset(Dataset):
     def __init__(self, cfg, train=True):
@@ -30,7 +32,15 @@ class SpecDataset(Dataset):
         self.rawNameList = trainFiles if train else testFiles
         # self.rawNameList = mfiles.filesNoExt
         self.rawNameList = [n[:-8] for n in self.rawNameList]
-        # self.max_length = max_length
+
+        #import ground truth data
+        self.gt_df = pd.read_excel(os.path.join(cfg['DATASET']['DATA_DIR'],cfg['DATASET']['GROUND_TRUTH']),
+                                usecols=[0,1,2,3,9,11,12])
+
+        self.max_width = cfg["DATASET"]["MAX_WIDTH"]
+        self.max_length = cfg["DATASET"]["MAX_LENGTH"]
+
+
 
     def __len__(self): 
         return len(self.rawNameList)
@@ -44,14 +54,27 @@ class SpecDataset(Dataset):
         pst = np.load(self.root_dir + r'/' + self.rawNameList[idx] + '_imgPst.npy')
         #white reference calibration
         image = caliColor(image, whiteRef)
+        #pad/cut image to the same size
+        if (image.shape[1] < self.max_length):
+            image = np.pad(image, ((0, 0),(self.max_length - image.shape[1], 0),(0,0)))
+        else:
+            image = image[:,image.shape[1]-self.max_length:,:]
+        
         #get mask
         ndvi = getNDVIHeatmap(image, self.cfg['HSI']['WV2PST'])
+        plt.imshow(ndvi)
+        plt.show()
         mask = getMask(image, ndvi, self.cfg['HSI']['NDVI_THRESH'])
         #find index of the spectrum that got extracted.
         location = np.where(mask)
         #extract spectrum that is part of the leaf
-        specs = image[mask]
-        specs = torch.tensor(specs, dtype=torch.float32)
+        # specs = image[mask]
+        # specs = torch.tensor(specs, dtype=torch.float32)
+        
+        #get ground truth data
+        leaf_num = int(self.rawNameList[idx].split('_')[1])
+        gt = self.gt_df.iloc[leaf_num-1, 4:7].to_numpy().astype('float32')
+        gt = torch.tensor(gt, dtype=torch.float32)
         
         # t0 = time.time()
         # reconstruct = torch.tensor(np.zeros_like(image), dtype=torch.float32)
@@ -71,12 +94,12 @@ class SpecDataset(Dataset):
         # image = F.pad(image, (0, self.max_length-image.shape[2],0,self.max_width-image.shape[1]))
         # if self.transform:
         #     image = self.transform(image)
-        return (specs, pst, location, image.shape)
+        return (image, mask, pst, location, image.shape, gt)
 
 if __name__ == "__main__":
-    cfg = yaml.load(open('config/config.yaml'), Loader=yaml.FullLoader)
+    cfg = yaml.load(open('config/config_test.yaml'), Loader=yaml.FullLoader)
     dataset = SpecDataset(cfg, True)
 
     for i in range(len(dataset)):
         sample = dataset[i]
-        print(i, sample[0].size())
+        print(i, sample[0].shape, dataset[i][4])
