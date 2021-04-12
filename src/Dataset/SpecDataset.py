24829c9e0926c74b3f758c4a402ed0bfd87cf86c
Jerry Wang <jerrywangpico@gmail.com>
2021-04-12 16:20:09 -0400

    Complete Spctral Model Training

--- src/Dataset/SpecDataset.py
+++ src/Dataset/SpecDataset.py
@@ -1,17 +1,20 @@
+import matplotlib.pyplot as plt
+import os
+import yaml
+import pandas as pd
+from Dataset.utils.HSI_Analysis import caliColor, getMask, getNDVIHeatmap
+from Dataset.utils.files import Files
+import numpy as np
+from torchvision import transforms, utils
+from torch.utils.data import Dataset
+import torch.nn.functional as F
+import torch
 import time
 import sys
-sys.path.insert(0, '/home/jerry/Documents/Research/HSI_Deep_Learning_Modeling/')
-import torch
-import torch.nn.functional as F
-from torch.utils.data import Dataset
-from torchvision import transforms, utils
-import numpy as np
-from Dataset.utils.files import Files
-from Dataset.utils.HSI_Analysis import caliColor, getMask, getNDVIHeatmap
-import pandas as pd
-import yaml
-import os
-import matplotlib.pyplot as plt
+
+sys.path.insert(
+    0, '/home/jerry/Documents/Research/HSI_Deep_Learning_Modeling/')
+
 
 class SpecDataset(Dataset):
     def __init__(self, cfg, train=True):
@@ -33,49 +36,50 @@ class SpecDataset(Dataset):
         # self.rawNameList = mfiles.filesNoExt
         self.rawNameList = [n[:-8] for n in self.rawNameList]
 
-        #import ground truth data
-        self.gt_df = pd.read_excel(os.path.join(cfg['DATASET']['DATA_DIR'],cfg['DATASET']['GROUND_TRUTH']),
-                                usecols=[0,1,2,3,9,11,12])
+        # import ground truth data
+        self.gt_df = pd.read_excel(os.path.join(cfg['DATASET']['DATA_DIR'], cfg['DATASET']['GROUND_TRUTH']),
+                                   usecols=[0, 1, 2, 3, 9, 11, 12])
 
         self.max_width = cfg["DATASET"]["MAX_WIDTH"]
         self.max_length = cfg["DATASET"]["MAX_LENGTH"]
 
-
-
-    def __len__(self): 
+    def __len__(self):
         return len(self.rawNameList)
-    
+
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        #load images
+        # load images
         image = np.load(self.root_dir + r'/' + self.rawNameList[idx] + '.npy')
-        whiteRef = np.load(self.root_dir + r'/' + self.rawNameList[idx] + '_whRef.npy')
-        pst = np.load(self.root_dir + r'/' + self.rawNameList[idx] + '_imgPst.npy')
-        #white reference calibration
+        whiteRef = np.load(self.root_dir + r'/' +
+                           self.rawNameList[idx] + '_whRef.npy')
+        pst = np.load(self.root_dir + r'/' +
+                      self.rawNameList[idx] + '_imgPst.npy')
+        # white reference calibration
         image = caliColor(image, whiteRef)
-        #pad/cut image to the same size
+        # pad/cut image to the same size
         if (image.shape[1] < self.max_length):
-            image = np.pad(image, ((0, 0),(self.max_length - image.shape[1], 0),(0,0)))
+            image = np.pad(
+                image, ((0, 0), (self.max_length - image.shape[1], 0), (0, 0)))
         else:
-            image = image[:,image.shape[1]-self.max_length:,:]
-        
-        #get mask
+            image = image[:, image.shape[1]-self.max_length:, :]
+
+        # get mask
         ndvi = getNDVIHeatmap(image, self.cfg['HSI']['WV2PST'])
         plt.imshow(ndvi)
         plt.show()
         mask = getMask(image, ndvi, self.cfg['HSI']['NDVI_THRESH'])
-        #find index of the spectrum that got extracted.
-        location = np.where(mask)
-        #extract spectrum that is part of the leaf
+        # find index of the spectrum that got extracted.
+        # location = np.where(mask)
+        # extract spectrum that is part of the leaf
         # specs = image[mask]
         # specs = torch.tensor(specs, dtype=torch.float32)
-        
-        #get ground truth data
+
+        # get ground truth data
         leaf_num = int(self.rawNameList[idx].split('_')[1])
         gt = self.gt_df.iloc[leaf_num-1, 4:7].to_numpy().astype('float32')
         gt = torch.tensor(gt, dtype=torch.float32)
-        
+
         # t0 = time.time()
         # reconstruct = torch.tensor(np.zeros_like(image), dtype=torch.float32)
         # k = 0
@@ -86,7 +90,6 @@ class SpecDataset(Dataset):
         #     k += 1
         # t1 = time.time()
         # print(t1-t0)
-        
 
         # specs = specs.permute((1,0))
         # image = torch.tensor(image, dtype=torch.float32)
@@ -94,12 +97,14 @@ class SpecDataset(Dataset):
         # image = F.pad(image, (0, self.max_length-image.shape[2],0,self.max_width-image.shape[1]))
         # if self.transform:
         #     image = self.transform(image)
-        return {'hsi_img': image, 'mask': mask, 'pst': pst, 'shape': image.shape, 'gt': gt}
+        return {'hsi_img': torch.tensor(image, dtype=torch.float32), 'mask': mask, 'pst': pst, 'shape': image.shape, 'gt': gt}
+
 
 if __name__ == "__main__":
     cfg = yaml.load(open('config/config_test.yaml'), Loader=yaml.FullLoader)
     dataset = SpecDataset(cfg, True)
-    train_loader = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=True)
+    train_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=4, shuffle=True)
 
     # for i in range(len(dataset)):
     #     sample = dataset[i]
--- src/train_spec_AE.py
+++ src/train_spec_AE.py
@@ -1,16 +1,16 @@
+import datetime
+from model.Spec_AE import Spec_AE
+from Dataset.SpecDataset import SpecDataset
+import pprint
+import yaml
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.nn as nn
+import torch
 import os
 import sys
-sys.path.insert(0, '/home/jerry/Documents/Research/HSI_Deep_Learning_Modeling/')
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import yaml
-import pprint
-from Dataset.SpecDataset import SpecDataset
-from model.Spec_AE import Spec_AE
-import datetime
-import sys
+sys.path.insert(
+    0, '/home/jerry/Documents/Research/HSI_Deep_Learning_Modeling/')
 
 
 def train_spec_AE(cfg, model_path):
@@ -23,7 +23,7 @@ def train_spec_AE(cfg, model_path):
 
     best_loss = -1
     best_loss_epoch = -1
-    
+
     # transform_list = []
     # if cfg['DATASET']['FLIP_HORIZONTAL']:
     #     transform_list.append(transforms.RandomHorizontalFlip(p=0.5))
@@ -35,24 +35,24 @@ def train_spec_AE(cfg, model_path):
     #     transform_list.append(transforms.RandomRotation((-20, 20), expand=False))
 
     # transform_list = transforms.Compose(transform_list)
-        
-    AE = Spec_AE(cfg['MODEL']['SPEC_AE']['N_WAVELENGTH'], 
-                cfg['MODEL']['SPEC_AE']['N_LATENT'],
-                cfg['MODEL']['SPEC_AE']['N_STAGES']).cuda()
+
+    AE = Spec_AE(cfg['MODEL']['SPEC_AE']['N_WAVELENGTH'],
+                 cfg['MODEL']['SPEC_AE']['N_LATENT'],
+                 cfg['MODEL']['SPEC_AE']['N_STAGES']).cuda()
 
     print(AE)
 
     # if pre_train_model != None:
     #     print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + 'Load From exisiting model: ' + pre_train_model)
     #     AE.load_state_dict(torch.load(cfg['MODEL']['AE']['MODEL_PATH'] + '/' + pre_train_model))
-    
+
     dataset = SpecDataset(cfg=cfg, train=True)
 
     test_dataset = SpecDataset(cfg=cfg, train=False)
-    
-    train_loader = torch.utils.data.DataLoader(dataset, 
-                                                batch_size=1,
-                                                shuffle=True)
+
+    train_loader = torch.utils.data.DataLoader(dataset,
+                                               batch_size=1,
+                                               shuffle=True)
 
     optimizer = optim.Adam(AE.parameters(), lr=cfg['OPTIMIZER']['lr'])
     loss_fn = nn.MSELoss(reduction='mean')
@@ -61,8 +61,7 @@ def train_spec_AE(cfg, model_path):
         AE.train()
         for batch_idx, data in enumerate(train_loader):
             # hsi_img = data[0]
-            # specs = rearrageData(hsi_img)
-            specs = torch.flatten(data[0], start_dim=0, end_dim=1)
+            specs = torch.flatten(data['hsi_img'], start_dim=0, end_dim=2)
             num_batch = int(specs.shape[0] / float(batch_size))
             train_loss_list = []
             for i in range(num_batch):
@@ -73,29 +72,32 @@ def train_spec_AE(cfg, model_path):
                 loss.backward()
                 optimizer.step()
                 train_loss_list.append(loss.item())
-                
-            print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + f' Epoch {epoch}: [{batch_idx}/{len(train_loader.dataset)}] Loss: {sum(train_loss_list)/len(train_loss_list)}')
+
+            print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") +
+                  f' Epoch {epoch}: [{batch_idx}/{len(train_loader.dataset)}] Loss: {sum(train_loss_list)/len(train_loss_list)}')
         val_loss = validation(epoch, cfg, AE, test_dataset)
         if best_loss == -1 or best_loss > val_loss:
-            print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + f' Epoch {epoch}: Better loss -- Save Model')
+            print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") +
+                  f' Epoch {epoch}: Better loss -- Save Model')
             best_loss = val_loss
             best_loss_epoch = epoch
             torch.save(AE.state_dict(), model_path)
-    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + f' Best Loss: {best_loss} @ Epoch {best_loss_epoch}')
-    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' Best Model Saved at: ' + model_path)
-    
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") +
+          f' Best Loss: {best_loss} @ Epoch {best_loss_epoch}')
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") +
+          ' Best Model Saved at: ' + model_path)
+
     return time
 
-    
 
 def validation(epoch, cfg, model, dataset):
     batch_size = cfg['TEST']['BATCH_SIZE']
 
     model.eval()
-    
-    test_loader = torch.utils.data.DataLoader(dataset, 
-                                                batch_size=batch_size,
-                                                shuffle=False)
+
+    test_loader = torch.utils.data.DataLoader(dataset,
+                                              batch_size=batch_size,
+                                              shuffle=False)
 
     loss_fn = nn.MSELoss(reduction='mean')
     test_loss = 0
@@ -105,19 +107,23 @@ def validation(epoch, cfg, model, dataset):
             specs = specs.cuda()
             _, output = model(specs)
             test_loss += loss_fn(output, specs).item()
-            print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + f' Validation @ Epoch {epoch}: [{i}/{len(test_loader.dataset)}] Loss: {loss_fn(output, specs).item()}')
+            print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") +
+                  f' Validation @ Epoch {epoch}: [{i}/{len(test_loader.dataset)}] Loss: {loss_fn(output, specs).item()}')
         test_loss /= len(test_loader.dataset)
-        print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + f' Epoch {epoch}: Test result on the model: Avg Loss is {test_loss}')
+        print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") +
+              f' Epoch {epoch}: Test result on the model: Avg Loss is {test_loss}')
     return test_loss
-    
+
 # def rearrageData(x):
 #     x = torch.flatten(x, start_dim=2)
 #     x = torch.flatten(x, start_dim=0, end_dim=1)
 #     x = x.permute(1, 0)
 #     return x
 
+
 if __name__ == "__main__":
     args = sys.argv
-    cfg = yaml.load(open('/home/jerry/Documents/Research/HSI_Deep_Learning_Modeling/config/' + args[1]), Loader=yaml.FullLoader)
+    cfg = yaml.load(open(
+        '/home/jerry/Documents/Research/HSI_Deep_Learning_Modeling/config/' + args[1]), Loader=yaml.FullLoader)
     pprint.pprint(cfg, indent=2)
     print(train_spec_AE(cfg))
